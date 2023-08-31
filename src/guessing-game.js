828d5899d337a3ffb0ea7d49eb9690bed7237954
Svetlana Kovalenko <kovalenkohp@mail.ru>
2023-08-31 15:25:43 +0400

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
+    arr = []
+    min;
+    max;
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
+        for (let i = min; i <= max; i++) {
+            this.arr.push(i);
+        }
     }
 
     guess() {
-
+        let middle = Math.round((this.max  + this.min) / 2)
+        return middle
     }
 
     lower() {
-
+        let middle = Math.round((this.max  + this.min) / 2)
+        this.max = middle
     }
 
     greater() {
-
+        let middle = Math.round((this.max  + this.min) / 2)
+        this.min = middle
     }
 }
 
